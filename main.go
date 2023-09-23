1921da2dc2776855e527b847cc75982221a96ac1
Jose Hurtado Baeza <josehurtadobaeza@gmail.com>
2023-09-23 22:23:17 +0200

    Arrays & Slices

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
+	//var bookings [50]string // ARRAY
+	//var bookings = [50]string{"Jose", "Carlos", "Pepe"} // ARRAY
+	var bookings []string // SLICE
+	//var bookings []string{} // Alternative way to declare a slice
+	//bookings := []string{} // Alternative way to declare a slice
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -32,8 +39,22 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
+	// bookings[0] = firstName + " " + lastName // Working with arrays
+	bookings = append(bookings, firstName+" "+lastName) // Working with slices
+
+	// fmt.Printf("The whole array: %v\n", bookings)
+	// fmt.Printf("The first value: %v\n", bookings[0])
+	// fmt.Printf("Array type: %T\n", bookings)
+	// fmt.Printf("Array length: %v\n", len(bookings))
+
+	// fmt.Printf("The whole slice: %v\n", bookings)
+	// fmt.Printf("The first value: %v\n", bookings[0])
+	// fmt.Printf("Slice type: %T\n", bookings)
+	// fmt.Printf("Slice length: %v\n", len(bookings))
 
 	fmt.Printf("Thank your %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings: %v\n", bookings)
 
 }
